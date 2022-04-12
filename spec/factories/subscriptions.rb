480671dd699f2c0fc4146044f2469e3b8e79666b
Kevin Gloss <kgloss89@gmail.com>
2022-04-12 10:59:04 -0600

    Create factories

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :subscription do
     customer
-    price { Faker::Number.number(digits: 5) }
     status { Faker::Number.within(range: 0..2) }
     frequency { Faker::Number.within(range: 0..2) }
   end
