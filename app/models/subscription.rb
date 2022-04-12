374403ca43394a5c79c81c770bd48c50fcd331f5
Kevin Gloss <kgloss89@gmail.com>
2022-04-12 16:14:07 -0600

    Update status enum

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,6 @@ class Subscription < ApplicationRecord
   has_many :teas, through: :tea_subscriptions
   validates :status, presence: true
   validates :frequency, presence: true
-  enum status: { pending: 0, active: 1, cancelled: 2 }
+  enum status: { active: 0, cancelled: 1 }
   enum frequency: { weekly: 0, bi_weekly: 1, monthly: 2 }
 end
