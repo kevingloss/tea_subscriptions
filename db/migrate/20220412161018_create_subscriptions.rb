a8ee2bb995acf3cbf994020a540ba08db8d69425
Kevin Gloss <kgloss89@gmail.com>
2022-04-12 12:39:38 -0600

    Add default value to frequency and status

--- db/migrate/20220412161018_create_subscriptions.rb
+++ db/migrate/20220412161018_create_subscriptions.rb
@@ -2,8 +2,8 @@ class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
       t.float :price
-      t.integer :status
-      t.integer :frequency
+      t.integer :status, default: 0
+      t.integer :frequency, default: 0
       t.references :customer, foreign_key: true
 
       t.timestamps
