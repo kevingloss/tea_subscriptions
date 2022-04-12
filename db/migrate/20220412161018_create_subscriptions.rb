10bd1e468d7fa2bb9807ebdf2f571339704b815c
Kevin Gloss <kgloss89@gmail.com>
2022-04-12 10:14:03 -0600

    Create subscriptions table

---
+++ db/migrate/20220412161018_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.float :price
+      t.integer :status
+      t.integer :frequency
+      t.references :customer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
