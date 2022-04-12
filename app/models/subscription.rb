0c90f87e07e770000fa731c7fd9f52f4b6085fc3
Kevin Gloss <kgloss89@gmail.com>
2022-04-12 16:04:58 -0600

    Fix typo

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,6 @@ class Subscription < ApplicationRecord
   has_many :teas, through: :tea_subscriptions
   validates :status, presence: true
   validates :frequency, presence: true
-  enum status: { pending: 0, active: 1, canceled: 2 }
+  enum status: { pending: 0, active: 1, cancelled: 2 }
   enum frequency: { weekly: 0, bi_weekly: 1, monthly: 2 }
 end
